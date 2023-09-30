f1be9ac73cd8918ee7b2c6b9f09a8c251a641702
ZDeanzo <47153579+ZDeanzo@users.noreply.github.com>
2023-09-30 10:42:27 +0100

    Update convert.sh
    Seems to work. Need to test on live data to see.
    
    To Do.
    
    write JAMF headers at the start of the file.
    
    Seems to fail when using sed and other methods on me

--- convert.sh
+++ convert.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+echo ""
+echo "=============  The input spreasheet should have the following format:  ============="
+echo "CLASS | ADMISSION NO. | FIRST NAME | LAST NAME | EMAIL | HOUSE | CLASS TEACHER EMAIL"
+echo "===================================================================================="
+echo ""
+
 # Set the FILE equal to the next command argument, i.e., $./SCRIPT.sh FILE.csv
 FILE=${1}
+
+
+# Remove the header from the CSV file.
+tail -n +2 "$FILE" > /tmp/HEADLESS_FILE.csv
+
+# Sort the CSV file by email.
+sort -t, -k5 < /tmp/HEADLESS_FILE.csv > /tmp/SORTED_FILE.csv
+
 
 # Set the Field Separator to read line by line
 IFS=$'\n'
@@ -10,7 +24,7 @@ PRE_EMAIL=""
 COMBINED=""
 NEWFILE=Converted.csv
 
-for line in $(cat "${FILE}")
+for line in $(cat "/tmp/SORTED_FILE.csv")
 do
     # Set Field Separator to a comma and create variables for each column
     IFS=','
@@ -29,7 +43,7 @@ do
        
     else
         #Export the prevous student to the csv file
-        echo "Outputting $PRE_USERNAME,$PRE_EMAIL,$PRE_FIRST,$PRE_LAST,\"$COMBINED\",, to CSV"
+        echo "Outputting $PRE_USERNAME to CSV"
         echo "$PRE_USERNAME,$PRE_EMAIL,$PRE_FIRST,$PRE_LAST,\"$COMBINED\",," >> output.csv
         COMBINED="$CLASS"
         
@@ -43,5 +57,11 @@ do
 done
 
 #Export the last student to the csv file as they are missed by the loop
-echo "Outputting $PRE_USERNAME,$PRE_EMAIL,$PRE_FIRST,$PRE_LAST,\"$COMBINED\",, to CSV"
+echo "Outputting $PRE_USERNAME to CSV"
 echo "$PRE_USERNAME,$PRE_EMAIL,$PRE_FIRST,$PRE_LAST,\"$COMBINED\",," >> output.csv
+
+echo ""
+echo "===================================================================================="
+echo "================  Task completed. Jamf headers need to be inseted  ================="
+echo "===================================================================================="
+echo ""
